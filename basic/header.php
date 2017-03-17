9c21b66fe6b346201dd0ccec7a951e57b563c2ec
fad4470 <fad4470@rit.edu>
2017-03-17 02:28:07 -0400

    Fix adaptive logo

--- basic/header.php
+++ basic/header.php
@@ -30,7 +30,8 @@
 
                     do_action( 'basic_before_blogname_in_logo' );
                   {?>
-					<img alt="<?php bloginfo('name'); ?>" src="<?php bloginfo('stylesheet_directory'); ?>/img/Toontown-logo-spun.png"/>
+					<img alt="<?php bloginfo('name'); ?>" src="<?php bloginfo('stylesheet_directory'); ?>/img/Toontown-logo-spun.png" class="web"/>
+					<img alt="<?php bloginfo('name'); ?>" src="<?php bloginfo('stylesheet_directory'); ?>/img/Toontown-logo.png" class="mobile"/>
                   <?php }
 					do_action( 'basic_after_blogname_in_logo' );
 
